96242168bbf6ff81bdb352c0c1f4bfa84f04e1b1
Larissa Sales <larissa.s.sales@outlook.com>
2020-02-11 10:39:32 -0300

    Add lista 5 - pilhas e filas

---
+++ listas/lista-5/b-pilha-desempilha-vetor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pilha{
+    int *dados;
+    int N, topo;
+} pilha;
+
+int desempilha(pilha *p, int *y){
+    if(p->topo == NULL || p->dados == NULL) return 0;
+
+    p->topo--;
+    *y = p->dados[p->topo];
+    return 1;
+}
